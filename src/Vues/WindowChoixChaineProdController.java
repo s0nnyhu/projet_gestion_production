3881710af05aa21986fcf86a203ea7a5040bd560
Sonny Hu <Sonny.hu@toulouse.miage.fr>
2019-02-16 18:46:23 +0100

    Ajouts des deux nouveaux interfaces necessaires a linterface finale

---
+++ src/Vues/WindowChoixChaineProdController.java
@@ -0,0 +1,30 @@
+package Vues;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+
+public class WindowChoixChaineProdController {
+
+    @FXML
+    private TableColumn<?, ?> colChoisir;
+
+    @FXML
+    private TableColumn<?, ?> colCode;
+
+    @FXML
+    private TableColumn<?, ?> colEntree;
+
+    @FXML
+    private TableColumn<?, ?> colSortie;
+
+    @FXML
+    private TableColumn<?, ?> colTemps;
+
+    @FXML
+    private Button btnSuivant;
+
+    @FXML
+    private Button btnAnnuler;
+
+}
